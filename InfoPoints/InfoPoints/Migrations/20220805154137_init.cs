72e707a6a196c9c8cb70387de56376958c0a5445
Радмир Гумеров <radmirgk@mail.ru>
2022-08-05 18:44:03 +0300

    добавлены миграции

---
+++ InfoPoints/InfoPoints/Migrations/20220805154137_init.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace InfoPoints.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "info");
+
+            migrationBuilder.CreateTable(
+                name: "notes",
+                schema: "info",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    comment = table.Column<string>(type: "text", nullable: true),
+                    color = table.Column<string>(type: "text", nullable: true),
+                    point_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "points",
+                schema: "info",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    x_axis = table.Column<int>(type: "integer", nullable: false),
+                    y_axis = table.Column<int>(type: "integer", nullable: false),
+                    radius = table.Column<int>(type: "integer", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_points", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notes",
+                schema: "info");
+
+            migrationBuilder.DropTable(
+                name: "points",
+                schema: "info");
+        }
+    }
+}
